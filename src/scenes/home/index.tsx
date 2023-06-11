cfcc21b0a030c88d264c992b54b1ca76d40a8a52
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-11 20:54:52 +0300

    Home page in progress 1

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -6,6 +6,7 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -15,25 +16,61 @@ const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
   return (
-    <section
-      id="home"
-      className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
-    >
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* Image and Main Header */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* Main Header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
+          <div className="md:-mt-20">
+            <div>
+              <div>
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              gravida scelerisque turpis, in ornare arcu interdum sed.
+              Suspendisse ullamcorper dignissim lectus, eget tristique leo
+              maximus ac. 
+            </p>
+          </div>
+
+          {/* Actions */}
           <div>
-            
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink 
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
           </div>
         </div>
 
         {/* Image */}
-        <div></div>
+        <div>
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        </div>
       </div>
+
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
+        <div>
+          <div>
+            <div>
+              <img src={SponsorRedBull} alt="redbull-sponsor" />
+              <img src={SponsorForbes} alt="forbes-sponsor" />
+              <img src={SponsorFortune} alt="fortune-sponsor" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
-  )
+  );
 };
 
 export default Home;
