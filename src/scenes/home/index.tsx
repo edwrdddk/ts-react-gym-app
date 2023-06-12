763f7955dc5d7c3e8234e8bbbaba75c3a5af9b2d
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-12 18:55:30 +0300

    Home page in progress 1.5 animation

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -20,9 +21,19 @@ const Home = ({ setSelectedPage }: Props) => {
       {/* Image and Main Header */}
       <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* Main Header */}
-        <div className="z-10 mt-32 md:basis-3/5">
+        <div className="z-10 mt-32 max-[768px]:mt-[100px] md:basis-3/5">
           {/* Headings */}
-          <div className="md:-mt-20">
+          <motion.div 
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
@@ -34,10 +45,20 @@ const Home = ({ setSelectedPage }: Props) => {
               Suspendisse ullamcorper dignissim lectus, eget tristique leo
               maximus ac. 
             </p>
-          </div>
+          </motion.div>
 
           {/* Actions */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div 
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay:0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -48,11 +69,11 @@ const Home = ({ setSelectedPage }: Props) => {
             >
               <p>Learn More</p>
             </AnchorLink>
-          </div>
+          </motion.div>
         </div>
 
         {/* Image */}
-        <div className="flex basis-[45%] justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+        <div className="flex basis-[60%] justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
       </div>
