770017f3f337c48d9cf730b0f2d6837864adfd90
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-15 19:37:30 +0300

    Benefits Page in progress 1.5 Benefit comp animation

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -29,6 +29,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -51,7 +58,13 @@ const Benefits = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* Benefits */}
-        <div className="mt-5 items-center justify-between gap-8 md:flex">
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit
               key={benefit.title}
@@ -61,7 +74,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           ))}
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
