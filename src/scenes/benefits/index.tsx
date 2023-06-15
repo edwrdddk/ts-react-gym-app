e2e07675b4e8b9f24d867dea623514f62a3349a1
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-15 20:40:08 +0300

    Benefits Page in progress 1.8 Graphics and Description: Title, Text, Button

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,3 +1,4 @@
+import ActionButton from "@/shared/ActionButton";
 import HText from "@/shared/Htext";
 import { BenefitType, SelectedPage } from "@/shared/types";
 import {
@@ -94,10 +95,41 @@ const Benefits = ({ setSelectedPage }: Props) => {
           {/* Description */}
           <div>
             {/* Title */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+                <HText>
+                  MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                  <span className="text-primary-500">FIT</span>
+                </HText>
+              </div>
+            </div>
 
             {/* Text */}
+            <div>
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </div>
 
             {/* Button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
           </div>
         </div>
       </motion.div>
