2030a4202abd44aacb0a3988bba621d1bd840031
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-17 20:24:33 +0300

    OurClasses Page in progress 1.0

---
+++ src/scenes/ourClasses/index.tsx
@@ -0,0 +1,14 @@
+import { SelectedPage } from '@/shared/types';
+import React from 'react'
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+}
+
+const OurClasses = ({ setSelectedPage }: Props) => {
+  return (
+    <div>OurClasses</div>
+  )
+}
+
+export default OurClasses
