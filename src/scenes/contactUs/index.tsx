86ef5595fcdbeda51ebd654ff7e3bec6d7f0655f
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-19 19:04:28 +0300

    ContactUs Page in progress 1.2 form name and email inputs

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,6 +9,21 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
   return (
     <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
       <motion.div
@@ -35,6 +50,60 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
           </p>
         </motion.div>
+
+        {/* Form and Image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formsubmit.co/edlollipopdidyk@gmail.com"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="NAME "
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 char."}
+                </p>
+              )}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="EMAIL "
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" && "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
