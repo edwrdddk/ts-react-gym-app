684d83c6f3f602452865a33ac9df97fdca0d3f6b
Edward Didyk <eduard.didyk01@gmail.com>
2023-06-19 19:10:33 +0300

    ContactUs Page in progress 1.4 submit button

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -122,6 +122,13 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                     "Max length is 2000 char."}
                 </p>
               )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
             </form>
           </motion.div>
         </div>
